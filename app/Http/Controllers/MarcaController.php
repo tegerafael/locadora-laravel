7b490ef4ef2e0767438e31ee7eccaadfdc1c3ac7
Tiago Rafael <tiagorafael777@gmail.com>
2023-10-06 16:03:52 -0400

    refactor: update crud processing

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -7,12 +7,16 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $marcas = Marca::all();
+        $marcas = $this->marca->all();
         return $marcas;
     }
 
@@ -29,15 +33,16 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        $marca = Marca::create($request->all());
+        $marca = $this->marca->create($request->all());
         return $marca;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Marca $marca)
+    public function show($id)
     {
+        $marca = $this->marca->find($id);
         return $marca;
     }
 
@@ -52,12 +57,9 @@ class MarcaController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
-        print_r($request->all());
-        echo '<hr>';
-        print_r($marca->getAttributes());
-
+        $marca = $this->marca->find($id);
         $marca->update($request->all());
         return $marca;
     }
@@ -65,8 +67,9 @@ class MarcaController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
+        $marca = $this->marca->find($id);
         $marca->delete();
         return ['msg' => 'A marca foi removida com sucesso'];
     }
