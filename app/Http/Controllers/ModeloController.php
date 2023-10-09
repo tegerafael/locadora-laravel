627b77f5a89c491c6f260864be10bbf748c8c91e
Tiago Rafael <tiagorafael777@gmail.com>
2023-10-09 19:26:26 -0400

    feat: add modelo CRUD

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -4,15 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Modelo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ModeloController extends Controller
 {
+    public function __construct(Modelo $modelo)
+    {
+        $this->modelo = $modelo;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $modelos = $this->modelo->all();
+        return response()->json($modelos, 200);
     }
 
     /**
@@ -28,15 +34,36 @@ class ModeloController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->modelo->rules());
+
+        $imagem = $request->file('imagem_mod');
+        $imagem_urn = $imagem->store('imagens/modelos', 'public');
+
+        $modelo = $this->modelo->create([
+            'nome_mod' => $request->nome_mod,
+            'imagem_mod' => $imagem_urn,
+            'numero_portas_mod' => $request->numero_portas_mod,
+            'lugares_mod' => $request->lugares_mod,
+            'air_bag_mod' => $request->air_bag_mod,
+            'abs_mod' => $request->abs_mod,
+            'id_mar_fk' => $request->id_mar_fk
+        ]);
+
+        return response()->json($modelo, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Modelo $modelo)
+    public function show($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if ($modelo === null) {
+            return response()->json(['erro' => 'Recurso pesquisado não existe'], 404);
+        }
+
+        return response()->json($modelo, 200);
     }
 
     /**
@@ -50,16 +77,63 @@ class ModeloController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Modelo $modelo)
+    public function update(Request $request, $id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if ($modelo === null) {
+            return response()->json(['erro' => 'Impossível realizar a atualização, o recurso solicitado não existe'], 404);
+        }
+
+        if ($request->method() === 'PATCH') {
+            $regrasDinamicas = array();
+
+            foreach ($modelo->rules() as $input => $regra) {
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+
+            $request->validate($regrasDinamicas);
+
+        } else {
+            $request->validate($modelo->rules());
+        }
+
+        if ($request->file('imagem_mod')) {
+            Storage::disk('public')->delete($modelo->imagem_mod);
+        }
+
+        $imagem = $request->file('imagem_mod');
+        $imagem_urn = $imagem->store('imagens/modelos', 'public');
+
+        $modelo->update([
+            'nome_mod' => $request->nome_mod,
+            'imagem_mod' => $imagem_urn,
+            'numero_portas_mod' => $request->numero_portas_mod,
+            'lugares_mod' => $request->lugares_mod,
+            'air_bag_mod' => $request->air_bag_mod,
+            'abs_mod' => $request->abs_mod,
+            'id_mar_fk' => $request->id_mar_fk
+        ]);
+
+        return response()->json($modelo, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Modelo $modelo)
+    public function destroy($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if ($modelo === null) {
+            return response()->json(['erro' => 'Impossível realizar a exclusão, o recurso solicitado não existe'], 404);
+        }
+
+        Storage::disk('public')->delete($modelo->imagem_mod);
+
+        $modelo->delete();
+        return response()->json(['msg' => 'O modelo foi removido com sucesso'], 200);
     }
 }
--- app/Models/Modelo.php
+++ app/Models/Modelo.php
@@ -10,4 +10,21 @@ class Modelo extends Model
     use HasFactory;
 
     protected $primaryKey = 'id_mod';
+
+    protected $fillable = ['nome_mod', 'imagem_mod', 'numero_portas_mod', 'lugares_mod', 'air_bag_mod', 'abs_mod', 'id_mar_fk'];
+
+    public function rules()
+    {
+        return [
+            'nome_mod' => 'required|unique:modelos,nome_mod,' . $this->id_mod . ',id_mod|min:3',
+            'imagem_mod' => 'required|file|mimes:png,jpeg,jpg',
+            'numero_portas_mod' => 'required|integer|digits_between:1,5',
+            'lugares_mod' => 'required|integer|digits_between:1,5',
+            'air_bag_mod' => 'required|boolean',
+            'abs_mod' => 'required|boolean',
+            'id_mar_fk' => 'exists:marcas,id_mar'
+        ];
+    }
+
+    // mensagens de feedback
 }
