6d7d5f0aa217168588b76dca5175503f8a523ab1
Tiago Rafael <tiagorafael777@gmail.com>
2023-10-13 11:34:33 -0400

    feat: implement jwt

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
-Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
-Route::apiResource('carro', 'App\Http\Controllers\CarroController');
-Route::apiResource('cliente', 'App\Http\Controllers\ClienteController');
-Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
+Route::middleware('jwt.auth')->group(function() {
+  Route::post('me', 'App\Http\Controllers\AuthController@me');
+  Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+  Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+  Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
+  Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
+  Route::apiResource('carro', 'App\Http\Controllers\CarroController');
+  Route::apiResource('cliente', 'App\Http\Controllers\ClienteController');
+  Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
+});
+
+Route::post('login', 'App\Http\Controllers\AuthController@login');
