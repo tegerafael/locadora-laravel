7f4228f9020030c0478d15ff5689e4de624e46ab
Tiago Rafael <tiagorafael777@gmail.com>
2023-10-13 09:00:11 -0400

    feat: add car classes

--- app/Http/Controllers/CarroController.php
+++ app/Http/Controllers/CarroController.php
@@ -2,18 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreCarroRequest;
-use App\Http\Requests\UpdateCarroRequest;
 use App\Models\Carro;
+use App\Repositories\CarroRepository;
+use Illuminate\Http\Request;
 
 class CarroController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function __construct(Carro $carro)
     {
-        //
+        $this->carro = $carro;
+    }
+    public function index(Request $request)
+    {
+        $carroRepository = new CarroRepository($this->carro);
+
+        if ($request->has('atributos_modelo')) {
+            $atributos_modelo = 'modelo:id_mod,' . $request->atributos_modelo;
+            $carroRepository->selectAtributosRegistrosRelacionados($atributos_modelo);
+        } else {
+            $carroRepository->selectAtributosRegistrosRelacionados('modelo');
+        }
+
+        if ($request->has('filtro')){
+            $carroRepository->filtro($request->filtro);
+        }
+
+        if ($request->has('atributos')) {
+            $carroRepository->selectAtributos($request->atributos);
+        }
+
+        return response()->json($carroRepository->getResultado(), 200);
     }
 
     /**
@@ -27,17 +45,32 @@ class CarroController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreCarroRequest $request)
+    public function store(Request $request)
     {
-        //
+        $request->validate($this->carro->rules());
+
+        $carro = $this->carro->create([
+            'placa_car' => $request->placa_car,
+            'disponivel_car' => $request->disponivel_car,
+            'km_car' => $request->km_car,
+            'id_mod_fk' => $request->id_mod_fk
+        ]);
+
+        return response()->json($carro, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Carro $carro)
+    public function show($id)
     {
-        //
+        $carro = $this->carro->with('modelo')->find($id);
+
+        if ($carro === null) {
+            return response()->json(['erro' => 'Recurso pesquisado não existe'], 404);
+        }
+
+        return response()->json($carro, 200);
     }
 
     /**
@@ -51,16 +84,47 @@ class CarroController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCarroRequest $request, Carro $carro)
+    public function update(Request $request, $id)
     {
-        //
+        $carro = $this->carro->find($id);
+
+        if ($carro === null) {
+            return response()->json(['erro' => 'Impossível realizar a atualização, o recurso solicitado não existe'], 404);
+        }
+
+        if ($request->method() === 'PATCH') {
+            $regrasDinamicas = array();
+
+            foreach ($carro->rules() as $input => $regra) {
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+
+            $request->validate($regrasDinamicas);
+
+        } else {
+            $request->validate($carro->rules());
+        }
+
+        $carro->fill($request->all());
+        $carro->save();
+
+        return response()->json($carro, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Carro $carro)
+    public function destroy($id)
     {
-        //
+        $carro = $this->carro->find($id);
+
+        if ($carro === null) {
+            return response()->json(['erro' => 'Impossível realizar a exclusão, o recurso solicitado não existe'], 404);
+        }
+
+        $carro->delete();
+        return response()->json(['msg' => 'O carro foi removido com sucesso'], 200);
     }
 }
--- app/Models/Carro.php
+++ app/Models/Carro.php
@@ -10,4 +10,20 @@ class Carro extends Model
     use HasFactory;
 
     protected $primaryKey = 'id_car';
+
+    protected $fillable = ['placa_car', 'disponivel_car', 'km_car', 'id_mod_fk'];
+
+    public function rules()
+    {
+        return [
+            'placa_car' => 'required',
+            'disponivel_car' => 'required',
+            'km_car' => 'required',
+            'id_mod_fk' => 'exists:modelos,id_mod'
+        ];
+    }
+
+    public function modelo() {
+        return $this->belongsTo(Modelo::class, 'id_mod_fk');
+    }
 }
